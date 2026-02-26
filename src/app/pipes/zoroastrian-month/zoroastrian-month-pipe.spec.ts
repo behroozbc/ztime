fad00698a1f08a4a88957c67b0da6220639304e4
Behrooz <behroozbc@outlook.com>
2026-02-26 12:49:00 +0100

    init

--- src/app/pipes/zoroastrian-month/zoroastrian-month-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ZoroastrianMonthPipe } from './zoroastrian-month-pipe';
-
-describe('ZoroastrianMonthPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ZoroastrianMonthPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
